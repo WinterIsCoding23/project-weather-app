bc443ccc218e56bec23923000454e81d73c21252
Kilian Sprotte <kilian.sprotte@gmail.com>
2023-04-18 16:55:54 +0200

    Form creates activity on submit and passes it to onAddActivity

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
-import Form from './components/Form';
+import "./App.css";
+import Form from "./components/Form";
 
 function App() {
-  function handleAddActivity (){
+  function handleAddActivity(activity) {
     console.log("handleAddActivity");
+    console.log(activity);
   }
 
   return (
--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,21 +1,41 @@
+export default function Form({ onAddActivity }) {
+  function handleSubmit(event) {
+    event.preventDefault();
 
+    // const formData = new FormData(event.target);
+    // const data = Object.fromEntries(formData);
+    // const activity = {
+    //   name: data.name,
+    //   isForGoodWeather: data.isForGoodWeather ? true : false,
+    // };
 
-export default function Form ({onAddActivity}){
-    function handleSubmit(event){
-        event.preventDefault();
-        console.log("Clicked the button");
-    }
+    const formElements = event.target.elements;
 
-    return (
-        <div>
-            <h1>Add new Activity</h1>
-            <form>
-                <label htmlFor="name">Name:</label>
-                <input type="text" id="name"></input>
-                <label htmlFor="good-weather-activity">Good-weather activity:</label>
-                <input type="checkbox" id="good-weather-activity"></input>
-                <button onClick={handleSubmit} type="submit">Submit</button>
-            </form>
-        </div>
-    )
+    const activity = {
+      name: formElements.name.value,
+      isForGoodWeather: formElements.isForGoodWeather.checked,
+    };
+
+    onAddActivity(activity);
+
+    event.target.reset();
+    formElements.name.focus();
+  }
+
+  return (
+    <div>
+      <h1>Add new Activity</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Name:</label>
+        <input type="text" id="name" name="name" autoFocus></input>
+        <label htmlFor="good-weather-activity">Good-weather activity:</label>
+        <input
+          type="checkbox"
+          id="good-weather-activity"
+          name="isForGoodWeather"
+        ></input>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 }
