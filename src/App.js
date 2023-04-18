d9fc38d9b8dc08c6a230408db8ecec3f7ae642fa
Markus Winter <markusdominicwinter@hotmail.com>
2023-04-18 17:34:06 +0200

    render list with activities

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import "./App.css";
 import Form from "./components/Form";
+import List from "./components/List/Index";
+import { useState } from "react";
+import { uid } from "uid";
 
 function App() {
   function handleAddActivity(activity) {
-    console.log("handleAddActivity");
-    console.log(activity);
+    const id = uid();
+    const activityWithId = {...activity, id}
+    setActivities([...activities, activityWithId]);
   }
+
+  const [activities, setActivities] = useState([]);
 
   return (
     <div className="App">
@@ -13,6 +19,7 @@ function App() {
         <h1>Weather & Activities App</h1>
       </header>
       <main>
+        <List activities={activities} />
         <Form onAddActivity={handleAddActivity} />
       </main>
     </div>
