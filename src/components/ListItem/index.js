b6c004ad02bb97456b0bc01f2b2bc5fb868d0a35
Markus Winter <markusdominicwinter@hotmail.com>
2023-04-19 17:24:01 +0200

    add delete-symbol

--- src/components/ListItem/index.js
+++ src/components/ListItem/index.js
@@ -9,7 +9,7 @@ export default function ListItem({ activity, onDeleteActivity }) {
     <li className="list-item">
       <div className="list-item__activity">{activity.name}</div>
       <button className="list-item__button" onClick={handleClick}>
-        X
+        <img src={require("./icons8-delete-400.png")} style={{width:"28px"}} alt="Delete-button" />
       </button>
     </li>
   );
